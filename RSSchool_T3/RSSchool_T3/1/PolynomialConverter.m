54da7d10e34a55876d57286d978740e8ffd3ef67
Siarhei Burakouski <serge.burakovsky@gmail.com>
2020-04-05 00:08:43 +0300

    fix: add 1-2 exc

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,29 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    if (([numbers count] == 0) || !numbers) return nil;
+    NSString *result = @"";
+    long cnt = [numbers count];
+    for (long i=cnt; i>0; i--) {
+        long current = [[numbers objectAtIndex:cnt-i] longValue];
+        NSString *sign = (current > 0)? @"+" : @"-";
+        if (current>1 || current<-1) {
+            if ((i-1) <= 0) {
+                result = [result stringByAppendingFormat:@" %@ %ld", sign, labs(current)];
+            } else if ((i-1) == 1) {
+                result = [result stringByAppendingFormat:@" %@ %ldx", sign, labs(current)];
+            } else {
+                result = [result stringByAppendingFormat:@" %@ %ldx^%ld", sign, labs(current), i-1];
+            }
+        } else if (current == 1 || current == -1) {
+            result = [result stringByAppendingFormat:@" %@ x", sign];
+        }
+    }
+    if ([result hasPrefix:@" + "]) {
+        result = [result substringFromIndex:3];
+    } else {
+        [result stringByReplacingCharactersInRange:NSMakeRange(0, 3) withString:@"-"];
+    }
+    return result;
 }
 @end
--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,25 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    if (!array || [array count] != 2) {return nil;}
+    unsigned long long n = [[array objectAtIndex:1] longLongValue];
+    unsigned long long m = [[array objectAtIndex:0] longLongValue];
+    for (unsigned long long k=0; k<=n; k++) {
+        if (binomial(n, k) == m) return [NSNumber numberWithLongLong:k];
+    }
+    return nil;
 }
+
+unsigned long long binomial(unsigned long long n, unsigned long long k) {
+    unsigned long long c = 1, i;
+    if (k > n-k) // take advantage of symmetry
+        k = n-k;
+    for (i = 1; i <= k; i++, n--) {
+        if (c/i > UINT_MAX/n) // return 0 on overflow
+            return 0;
+        c = c / i * n + c % i * n / i;  // split c * n / i into (c / i * i + c % i) * n / i
+    }
+    return c;
+}
+
 @end
